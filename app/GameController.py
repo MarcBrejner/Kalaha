11bf73916a5604ac489ecefb544380484d1f0134
MarcBrejner <marc@kaabrejner.dk>
2022-03-14 16:52:23 +0100

    game controller logic

---
+++ app/GameController.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+class GameController:
+    def __init__(self, model):
+        self.model = model
+
+    def isGameOver(self):
+        if (self.model.gameState[0][:-1].sum() + self.model.gameState[0][:-1].sum()) > 0:
+            return False
+        if self.model.gameState[0][-1] > self.model.gameState[1][-1]:
+            print("Player one won")
+        else:
+            print("Player two won")
+        return True
+
+    def turn(self, cup, player):
+        if player != self.model.currentPlayerTurn:
+            return
+        if cup < 0 or cup > (self.model.gameState[player].size - 2):
+            return
+
+        shells = self.model.gameState[player][cup]
+        self.model.gameState[player][cup] = 0
+
+        while shells > 0:
+            cup += 1
+
+            self.model.gameState[player][cup] += 1
+
+            if cup >= (self.model.gameState[player].size-1):
+                # swap player array
+                player = 1 - player
+                cup = 0
+
+            shells -= 1
+        self.isGameOver()
+
+
+
+
+
+
+
+
+
--- app/gameModel.py
+++ app/gameModel.py
@@ -1,9 +1,8 @@
 import numpy as np
 
-
 class GameModel:
     def __init__(self):
-        self.gameState = np.array([4, 4, 4, 4, 4, 4, 0], [4, 4, 4, 4, 4, 4, 0])
+        self.gameState = np.array([[4, 4, 4, 4, 4, 4, 0], [4, 4, 4, 4, 4, 4, 0]])
         self.currentPlayerTurn = 0
 
     def get_game_state(self):
