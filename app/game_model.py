3e1a3ba954f0037d98fbf3554ff01aef11f33edf
MarcBrejner <marc@kaabrejner.dk>
2022-03-16 13:52:52 +0100

    fill with zeros when capturing shells at the end of game

--- app/game_model.py
+++ app/game_model.py
@@ -3,4 +3,4 @@ import numpy as np
 
 class GameModel:
     def __init__(self):
-        self.gameState = np.array([[4, 4, 4, 4, 4, 4, 0], [9, 4, 4, 4, 4, 9, 0]])
+        self.gameState = np.array([[4, 4, 4, 4, 4, 4, 0], [4, 4, 4, 4, 4, 4, 0]])
